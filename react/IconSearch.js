36938efc434229228fa43b0dc1edf4a6c8eba22d
Bruno Dias <bruno.dias@vtex.com.br>
2019-01-30 14:21:20 -0300

    Add storecomponents icons

---
+++ react/IconSearch.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Icon from './components/Icon'
+import { proptypes } from './components/propTypes'
+
+const IconSearch = ({ ...props }) => {
+  return (
+    <Icon id="hpa-search" {...props} />
+  )
+}
+
+IconSearch.propTypes = proptypes
+
+export default IconSearch
