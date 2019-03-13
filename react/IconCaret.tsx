a20e6378a8a080cdfcc1be18196cde414d15bba5
klynger <rafael.klynger@gmail.com>
2019-03-13 15:38:55 -0300

    Use classnames

--- react/IconCaret.tsx
+++ react/IconCaret.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import classnames from 'classnames'
 import Icon from './components/Icon'
 import { getOrientation } from './utils/helpers'
 
 const IconCaret = ({ orientation, thin = false, ...props }: CaretProps) => {
   const orientationModifier = getOrientation(orientation)
-  return <Icon id={`nav-${thin ? 'thin-' : ''}caret${orientationModifier}`} {...props} />
+  const id: string = classnames({
+    [`nav-thin-caret${orientationModifier}`]: thin,
+    [`nav-caret${orientationModifier}`]: !thin
+  })
+  return <Icon id={id} {...props} />
 }
 
 export default IconCaret
