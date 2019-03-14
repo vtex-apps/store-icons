aca0d01195985f6fac81b7eaf08c8a6613b6c597
Arthur Sampaio <arthur.sampaio.correia@ccc.ufcg.edu.br>
2019-03-14 10:28:30 -0300

    Add icon check

---
+++ react/IconCheck.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import Icon from './components/Icon'
+
+const IconCheck = (props: IconProps) => {
+  return <Icon id="sti-check--line" {...props} />
+}
+
+export default IconCheck
