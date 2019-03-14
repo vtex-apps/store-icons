697cff801d6602387fcc7d4486aa95ed3fdce111
Arthur Sampaio <arthur.sampaio.correia@ccc.ufcg.edu.br>
2019-03-14 11:05:16 -0300

    Add EnhacedIconProps

--- react/IconCheck.tsx
+++ react/IconCheck.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { getType } from './utils/helpers'
 
 import Icon from './components/Icon'
 
-const IconCheck = (props: IconProps) => {
-  return <Icon id="sti-check--line" {...props} />
+const IconCheck = ({ type, ...props }: EnhancedIconProps) => {
+  const typeModifier = getType(type)
+  return <Icon id={`sti-check${typeModifier}`} {...props} />
 }
+
+
 
 export default IconCheck
