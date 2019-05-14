e18195ab25cd611ed6a1ddab52a1978148bd6d3c
Lucas Cordeiro <ecdb.lucas@gmail.com>
2019-05-14 14:58:48 -0300

    Update IconClose.tsx

--- react/IconClose.tsx
+++ react/IconClose.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Icon from './components/Icon'
 import { getType } from './utils/helpers'
 
-const IconClose = ({ type, ...props }: EnhancedIconProps) => {
+const IconClose = ({ type = 'filled', ...props }: EnhancedIconProps) => {
   const typeModifier = getType(type)
   return <Icon id={`sti-close${typeModifier}`} {...props} />
 }
