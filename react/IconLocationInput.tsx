35dad5a7ae4880fae0aeb899e1c898e5867e3200
Joao Fidelis <joaogfidelis@gmail.com>
2019-07-02 14:33:08 -0300

    input location icon

---
+++ react/IconLocationInput.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+ import Icon from './components/Icon'
+
+ const IconLocationInput = (props: IconProps) => {
+  return <Icon id="mpa-location-input-marker" {...props} />
+}
+
+ export default IconLocationInput
