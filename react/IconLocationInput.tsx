65d7ba82ccce03509cfda6d6a3e054099162ece3
Joao Fidelis <joaogfidelis@gmail.com>
2019-07-02 17:36:33 -0300

    remove marker from name

--- react/IconLocationInput.tsx
+++ react/IconLocationInput.tsx
@@ -3,7 +3,7 @@ import React from 'react'
  import Icon from './components/Icon'
 
  const IconLocationInput = (props: IconProps) => {
-  return <Icon id="mpa-location-input-marker" {...props} />
+  return <Icon id="mpa-location-input" {...props} />
 }
 
  export default IconLocationInput
