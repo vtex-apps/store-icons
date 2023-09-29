ca5b1cbd68212a8d767f1d634f3da7cc947c4eda
Paulo Haderach <146485189+paulohaderach@users.noreply.github.com>
2023-09-29 17:19:08 -0300

    Create dangerfile.js

---
+++ action/dangerfile.js
@@ -0,0 +1,3 @@
+const reviewMyPR = () => {
+  warn(`:exclamation: Pull Request size seems relatively large. If Pull Request contains multiple changes, split each into separate PR for faster, easier review.`);
+}
